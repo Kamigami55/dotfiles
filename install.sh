ace622cbb5179a241504dfc70e789fc692b11494
Kamigami55 <samkami@icloud.com>
2017-12-13 17:39:37 +0800

    Add auto install script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+# Install zsh and oh-my-zsh if not installed
+if [ $(dpkg-query -W -f='${Status}' zsh 2>/dev/null | grep -c "ok installed") == 0 ]
+then
+    if [[ "$OSTYPE" == "linux-gnueabihf" ]] # For raspbien
+    then
+        sudo apt-get --force-yes --yes install zsh
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    fi
+fi
+
+# Install tmux  if not installed
+if [ $(dpkg-query -W -f='${Status}' tmux 2>/dev/null | grep -c "ok installed") == 0 ]
+then
+    if [[ "$OSTYPE" == "linux-gnueabihf" ]] # For raspbien
+    then
+        sudo apt-get --force-yes --yes install tmux
+    fi
+fi
+
+
+# Create file links
+cd ~/
+if [ -e .vimrc ]; then
+	mv .vimrc .vimrc-old
+fi
+ln -s ~/unix-settings/.vimrc .vimrc
+
+if [ -e .gitconfig ]; then
+	mv .gitconfig .gitconfig-old
+fi
+ln -s ~/unix-settings/.gitconfig .gitconfig
+
+if [ -e .tmux.conf ]; then
+	mv .tmux.conf .tmux.conf-old
+fi
+ln -s ~/unix-settings/.tmux.conf .tmux.conf
+
+if [ -e .tmux ]; then
+	mv .tmux .tmux-old
+fi
+ln -s ~/unix-settings/.tmux .tmux
+
+if [ -e .zshrc ]; then
+	mv .zshrc .zshrc-old
+fi
+ln -s ~/unix-settings/.zshrc .zshrc
+
